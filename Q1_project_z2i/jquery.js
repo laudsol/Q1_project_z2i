4cd079aff80a9f7c1e10ae41971d9149b594b256
Sol Laudon <blaudon1@gmail.com>
2017-05-03 18:14:25 -0600

    5.3.17 dummy var, API timed out, distilled API data, cannpt append API data

---
+++ Q1_project_z2i/jquery.js
@@ -0,0 +1,41 @@
+
+
+$("form").submit(function( event ) {
+  var inputObj = $(this).serializeArray();
+
+  event.preventDefault();
+  // var financeRequests = [];
+  // for (var key in inputObj) {
+  //   let ticker = inputObj[key].value;
+  //   if (ticker !== "") {
+  //     financeRequests.push($.getJSON('http://www.enclout.com/api/yahoo_finance/show.json?auth_token=xxxxxx&text='+ticker));
+  //   }
+  // }
+  // Promise.all(financeRequests).then(function (results) {
+  //   console.log(results);
+
+    var tempResults = [[{bid: '$100', ask: '$105', close: '$102', symbol:'aapl'}],[{bid: 'NA', ask: 'NA', close:'$200', symbol: 'googl'}]];
+
+    for (i = 0; i < tempResults.length; i++) {
+      var tempData = [];
+      var tempObj = tempResults[i][0];
+      var tempSymbol = tempObj.symbol;
+      var tempBid =  tempObj.bid;
+      var tempBidNum = parseInt(tempBid.substring(1),10);
+      var tempAsk =  tempObj.ask;
+      var tempAskNum = parseInt(tempAsk.substring(1),10);
+      var tempClose =  tempObj.close;
+      var tempCloseNum = parseInt(tempClose.substring(1),10);
+      var price = 0;
+
+      if (tempAskNum >= 0 && tempBidNum >= 0) {
+        price = (tempBidNum+tempAskNum)*0.5;
+      } else {
+        price = tempCloseNum;
+      }
+      tempData.push(tempSymbol);
+      tempData.push(price);
+      console.log(tempData);
+    }
+  // });
+});
