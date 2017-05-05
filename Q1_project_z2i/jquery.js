7ee0ed40852970175695c60ccd9bf9b605cb162f
Sol Laudon <blaudon1@gmail.com>
2017-05-05 15:29:02 -0600

    API, local storage, % input, $ input, price output, shares output

--- Q1_project_z2i/jquery.js
+++ Q1_project_z2i/jquery.js
@@ -1,50 +1,67 @@
-
+myStorage = localStorage;
+localData = JSON.parse(localStorage['data']);
+// console.log(localData[0][0]['symbol']);
+var allData = [];
 
 $("form").submit(function( event ) {
-  var inputObj = $(this).serializeArray();
-
+  var inputObj = $(this).serializeArray(); //all form data
+  var tickers = inputObj.filter(function(obj){  //finds tickers from form data
+    return obj['name'] == 'ticker';
+  });
+  var tempData = []; //takes prased JSON data for action
   event.preventDefault();
-  var allData = [];
-  // var financeRequests = [];
-  // for (var key in inputObj) {
-  //   let ticker = inputObj[key].value;
-  //   if (ticker !== "") {
-  //     financeRequests.push($.getJSON('http://www.enclout.com/api/yahoo_finance/show.json?auth_token=xxxxxx&text='+ticker));
-  //   }
-  // }
-  // Promise.all(financeRequests).then(function (results) {
-  //   console.log(results);
+  var financeRequests = [];
+  for (var key in tickers) {
+    let ticker = tickers[key].value;
+    if (ticker !== "") {
+      financeRequests.push($.getJSON('http://www.enclout.com/api/yahoo_finance/show.json?auth_token=xxxxxx&text='+ticker));
+    }
+  }
+  Promise.all(financeRequests).then(function (results) {
+    console.log(results);
+    //SET LOCAL STORAGE
+    // localStorage.setItem('data',JSON.stringify(results)); //local storage
+    // localData = JSON.parse(localStorage['data']);
+    // console.log(localData);
 
-    var tempResults = [[{bid: '$100', ask: '$105', close: '$102', symbol:'aapl'}],[{bid: 'NA', ask: 'NA', close:'$200', symbol: 'googl'}]];
+    for (i = 0; i < results.length; i++) { //extract price and ticker
 
-    for (i = 0; i < tempResults.length; i++) {
-      var tempData = [];
-      var tempObj = tempResults[i][0];
+      var tempObj = results[i][0];
       var tempSymbol = tempObj.symbol;
-      var tempBid =  tempObj.bid;
-      var tempBidNum = parseInt(tempBid.substring(1),10);
-      var tempAsk =  tempObj.ask;
-      var tempAskNum = parseInt(tempAsk.substring(1),10);
-      var tempClose =  tempObj.close;
-      var tempCloseNum = parseInt(tempClose.substring(1),10);
+      var tempBid =  parseFloat(tempObj.bid);
+      var tempAsk =  parseFloat(tempObj.ask);
+      var tempClose =  parseFloat(tempObj.close);
       var price = 0;
 
-      if (tempAskNum >= 0 && tempBidNum >= 0) {
-        price = (tempBidNum+tempAskNum)*0.5;
+
+      if (tempAsk >= 0 && tempBid >= 0) { //finds price by bid/ask OR close
+        price = (tempBid+tempAsk)*0.5;
       } else {
-        price = tempCloseNum;
+        price = tempClose;
       }
-      tempData.push(tempSymbol);
       tempData.push(price);
-      allData.push(tempData);
-
-    }
-  // });
-
-    $('form').find('.stock').each(function (i,e) {
+    }         // END PROMISE -> ACTIONS FROM HERE
+    $('form').find('.ticker_inp').each(function (i) { //appends price to page
       var element = $('<div>');
-      element.html(allData[i][1]);
+      element.html("price: $"+tempData[i]);
       $(this).append(element);
     });
 
+    var investment = parseFloat($("#dollar_inv").val());
+
+    var percent = inputObj.filter(function(obj){  //gets an array of % from form
+      return obj['name'] == 'percent';
+    });
+
+    $('form').find('.percent').each(function (i) { //
+      var percentNum = parseFloat(percent[i].value)/100;
+      let price = tempData[i]
+      shares = Math.floor((investment * percentNum)/price);
+      var element = $('<div>');
+      element.html(shares+" shares");
+      $(this).append(element);
+    });
+
+
+  }); //END API CALL
 });
