b051fab965713b2a4d23650711b2c15aa913e1ad
Sol Laudon <blaudon1@gmail.com>
2017-05-06 20:15:33 -0600

    api, simple share allocation, prevent greater than 100% allocation, basic css layout

--- Q1_project_z2i/jquery.js
+++ Q1_project_z2i/jquery.js
@@ -1,7 +1,7 @@
-
+var totalAllocation = 100;
 
 $("form").on('input',$('.percentVal'), function (event) { // listener for percent of allocation
-  var totalPercent = 0;
+  let totalPercent = 0;
 
   $('form').find('.percentVal').each(function (i) {
     var number = $(this).val();
@@ -11,6 +11,7 @@ $("form").on('input',$('.percentVal'), function (event) { // listener for percen
     }
     percent = parseFloat(number);
     totalPercent += percent;
+    totalAllocation = totalPercent;
   });
   $('.totalAllocation').html("Total allocation: "+totalPercent+"%");
 }); // END PERCENT LISTENER
@@ -25,56 +26,60 @@ $("form").submit(function( event ) {
     return obj['name'] == 'ticker';
   });
   var tempData = []; //takes parsed JSON data for action
-  // if ()
-  event.preventDefault();
-  var financeRequests = [];
-  for (var key in tickers) {
-    let ticker = tickers[key].value;
-    if (ticker !== "") {
-      financeRequests.push($.getJSON('http://www.enclout.com/api/yahoo_finance/show.json?auth_token=xxxxxx&text='+ticker));
-    }
-  }
-  Promise.all(financeRequests).then(function (results) {
-    console.log(results);
-    //SET LOCAL STORAGE
-    // localStorage.setItem('data',JSON.stringify(results)); //local storage
-    // localData = JSON.parse(localStorage['data']);
-    // console.log(localData);
-
-    for (i = 0; i < results.length; i++) { //extract price and ticker
-
-      var tempObj = results[i][0];
-      var tempSymbol = tempObj.symbol;
-      var tempBid =  parseFloat(tempObj.bid);
-      var tempAsk =  parseFloat(tempObj.ask);
-      var tempClose =  parseFloat(tempObj.close);
-      var tempPrice = 0;
-
-      if (tempAsk >= 0 && tempBid >= 0) { //finds price by bid/ask OR close
-        tempPrice = (tempBid+tempAsk)*0.5;
-      } else {
-        tempPrice = tempClose;
+  if (totalAllocation > 100) {
+    window.alert("allocation cannot exceed 100%")
+    return false;
+  } else {
+    event.preventDefault();
+    var financeRequests = [];
+    for (var key in tickers) {
+      let ticker = tickers[key].value;
+      if (ticker !== "") {
+        financeRequests.push($.getJSON('http://www.enclout.com/api/yahoo_finance/show.json?auth_token=xxxxxx&text='+ticker));
       }
-      var price = parseFloat(tempPrice.toFixed(2));
-      tempData.push(price);
+    }
+    Promise.all(financeRequests).then(function (results) {
+      console.log(results);
+      //SET LOCAL STORAGE
+      // localStorage.setItem('data',JSON.stringify(results)); //local storage
+      // localData = JSON.parse(localStorage['data']);
+      // console.log(localData);
 
-    }         // END PROMISE -> ACTIONS FROM HERE
-    $('form').find('.current_price').each(function (i) { //appends price to page
-      $(this).html("Price: $"+tempData[i]);
-    });
+      for (i = 0; i < results.length; i++) { //extract price and ticker
 
-    var investment = parseFloat($("#dollar_inv").val());
+        var tempObj = results[i][0];
+        var tempSymbol = tempObj.symbol;
+        var tempBid =  parseFloat(tempObj.bid);
+        var tempAsk =  parseFloat(tempObj.ask);
+        var tempClose =  parseFloat(tempObj.close);
+        var tempPrice = 0;
 
-    var percent = inputObj.filter(function(obj){  //gets an array of % from form
-      return obj['name'] == 'percent';
-    });
+        if (tempAsk >= 0 && tempBid >= 0) { //finds price by bid/ask OR close
+          tempPrice = (tempBid+tempAsk)*0.5;
+        } else {
+          tempPrice = tempClose;
+        }
+        var price = parseFloat(tempPrice.toFixed(2));
+        tempData.push(price);
 
-    $('form').find('.shares').each(function (i) { //
-      var percentNum = parseFloat(percent[i].value)/100;
-      let price = tempData[i]
-      shares = Math.floor((investment * percentNum)/price);
-      $(this).html("Recommended shares: "+shares);
-    });
+      }         // END PROMISE -> ACTIONS FROM HERE
+      $('form').find('.current_price').each(function (i) { //appends price to page
+        $(this).html("Price: $"+tempData[i]);
+      });
 
-  }); //END API CALL
+      var investment = parseFloat($("#dollar_inv").val());
+
+      var percent = inputObj.filter(function(obj){  //gets an array of % from form
+        return obj['name'] == 'percent';
+      });
+
+      $('form').find('.shares').each(function (i) { //
+        var percentNum = parseFloat(percent[i].value)/100;
+        let price = tempData[i]
+        shares = Math.floor((investment * percentNum)/price);
+        $(this).html("Recommended shares: "+shares);
+      });
+
+    }); //END API CALL
+  };
 });
