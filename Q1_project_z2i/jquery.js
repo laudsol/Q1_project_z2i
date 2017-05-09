9673d72c098a6415c12b5d567b0ef43f4f0bdb2f
Sol Laudon <blaudon1@gmail.com>
2017-05-09 16:24:50 -0600

    working full screen and mobile. added color to all.

--- Q1_project_z2i/jquery.js
+++ Q1_project_z2i/jquery.js
@@ -26,50 +26,59 @@ $("form").on('input',$('.allocationVal'), function (event) { // listener for per
 $('.addBtn').on('click',function(event){
   //Primary elements
   let elementRow = $('<div>').addClass("row");
-  let elementStock = $('<div>').addClass("stock col-md-10 col-md-offset-1");
+  let elementStock = $('<div>').addClass("stock col-md-10 col-md-offset-1 col-xs-12");
   //first - ticker
   let elementTicker = $('<div>').addClass("tickerDiv col-md-2 col-md-offset-1");
-  let elementLabelTicker = $('<div>').addClass("tickerText visible-sm").html('Ticker:');
-  let elementInputTicker = $('<input>').addClass("tickerInput inOut col-md-12 col-sm-6").attr('style','border:none;').attr('type','text').attr('name','ticker').attr('value','');
+  let elementLabelTicker = $('<div>').addClass("tickerText mobileActive col-xs-6").html('Ticker:');
+  let elementBoxTicker = $('<div>').addClass("col-md-12 col-xs-6");
+  let elementInputTicker = $('<input>').addClass("tickerInput col-md-12 inOut ltblu").attr('style','border:none;').attr('type','text').attr('name','ticker').attr('value','');
 
   $(elementTicker).append(elementLabelTicker);
-  $(elementTicker).append(elementInputTicker);
+  $(elementBoxTicker).append(elementInputTicker);
+  $(elementTicker).append(elementBoxTicker);
   $(elementStock).append(elementTicker);
 
   //second - allocation
-  let elementPercent = $('<div>').addClass("allocationDiv percent col-md-2");
-  let elementLabelPercent = $('<div>').addClass("allocationText visible-sm").html('Allocation:');
-  let elementInputPercent = $('<input>').addClass("allocationVal inOut col-md-12 col-sm-6").attr('style','border:none;').attr('type','text').attr('name','percent').attr('value','0%');
+  let elementPercent = $('<div>').addClass("allocationDiv col-md-2");
+  let elementLabelPercent = $('<div>').addClass("allocationText mobileActive col-xs-6").html('Allocation:');
+  let elementBoxPercent = $('<div>').addClass("col-md-12 col-xs-6");
+  let elementInputPercent = $('<input>').addClass("allocationVal col-md-12 inOut ltblu").attr('style','border:none;').attr('type','text').attr('name','percent').attr('value','0%');
 
   $(elementPercent).append(elementLabelPercent);
-  $(elementPercent).append(elementInputPercent);
+  $(elementBoxPercent).append(elementInputPercent);
+  $(elementPercent).append(elementBoxPercent);
   $(elementStock).append(elementPercent);
 
   //third  - current shares
   let elementCurShar = $('<div>').addClass("currentSharesDiv col-md-2");
-  let elementLabelCurShar = $('<div>').addClass("currentSharesText visible-sm").html('Current shares:');
-  let elementInputCurShar = $('<input>').addClass("currentSharesInput inOut col-md-12 col-sm-6").attr('style','border:none;').attr('type','text').attr('name','currentShares').attr('value','0');
+  let elementLabelCurShar = $('<div>').addClass("currentSharesText mobileActive col-xs-6").html('Current shares:');
+  let elementBoxCurShar = $('<div>').addClass("col-md-12 col-xs-6");
+  let elementInputCurShar = $('<input>').addClass("currentSharesInput col-md-12 inOut ltblu").attr('style','border:none;').attr('type','text').attr('name','currentShares').attr('value','0');
 
   $(elementCurShar).append(elementLabelCurShar);
-  $(elementCurShar).append(elementInputCurShar);
+  $(elementBoxCurShar).append(elementInputCurShar)
+  $(elementCurShar).append(elementBoxCurShar);
   $(elementStock).append(elementCurShar);
 
   //fourth - price
   let elementPrice = $('<div>').addClass("priceDiv col-md-2");
-  let elementPriceText = $('<div>').addClass("priceText visible-sm").html("<strong>Price:</strong>");
-  let elementPriceInput = $('<div>').addClass("currentPriceOutput inOut col-md-12 col-sm-6").html('--');
+  let elementPriceText = $('<div>').addClass("priceText mobileActive col-xs-6").html("Price:");
+  let elementBoxPrice = $('<div>').addClass("col-md-12 col-xs-6");
+  let elementPriceInput = $('<input>').addClass("currentPriceOutput col-md-12 inOut orange").attr('style','border:none').attr('value','--');
 
   $(elementPrice).append(elementPriceText);
-  $(elementPrice).append(elementPriceInput);
+  $(elementBoxPrice).append(elementPriceInput);
+  $(elementPrice).append(elementBoxPrice);
   $(elementStock).append(elementPrice);
 
   //fifth - recommended shares
   let elementRecomShares = $('<div>').addClass("sharesDiv col-md-2");
-  let elementRecomSharesText = $('<div>').addClass("sharesText visible-sm").html("<strong>Recommended shares:</strong>");
-  let elementRecomSharesOutput = $('<div>').addClass("sharesOutput inOut col-md-12 col-sm-6").html('--');
-
+  let elementRecomSharesText = $('<div>').addClass("sharesText mobileActive col-xs-6").html("Recommended shares:");
+  let elementBoxRcomShares = $('<div>').addClass("col-md-12 col-xs-6");
+  let elementRecomSharesOutput = $('<input>').addClass("sharesOutput col-md-12 inOut orange").attr('style', 'border:none').attr('value','--');
   $(elementRecomShares).append(elementRecomSharesText);
-  $(elementRecomShares).append(elementRecomSharesOutput);
+  $(elementBoxRcomShares).append(elementRecomSharesOutput)
+  $(elementRecomShares).append(elementBoxRcomShares);
   $(elementStock).append(elementRecomShares);
 
   // append all to form
@@ -133,10 +142,16 @@ $("form").submit(function( event ) {
 
       }         // END PROMISE -> ACTIONS FROM HERE
       $('form').find('.currentPriceOutput').each(function (i) { //appends price to page
-        $(this).html("$"+tempData[i]);
+        $(this).val("$"+tempData[i]);
       });
 
-      var investment = parseFloat($("#dollar_inv").val());
+      var invInput = $("#dollar_inv").val();
+
+      if (invInput.substring(0,1) == "$") {
+        invInput = invInput.substring(1,invInput.length);
+      }
+
+      var investment = parseFloat(invInput);
 
       var percent = inputObj.filter(function(obj){  //gets an array of % from form
         return obj['name'] == 'percent';
@@ -160,7 +175,7 @@ $("form").submit(function( event ) {
         let dollarTarget = (totalInvestedDollars*percentNum)-((Math.min(1,tempData.length-1))*(currentSharesArr[i]*price));
 
         shares = Math.max(Math.floor((dollarTarget)/price),0);
-        $(this).html(shares);
+        $(this).val(shares);
 
         let delta = 1-((shares*price)/dollarTarget); // get delta between dollars spent, and allocated dollars
         let readySymbol = symbols[i]
